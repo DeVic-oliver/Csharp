60ad36cfd743ec835c63ffbb1a15a11953ec89c9
Victor Matheus <matheuslima.o183@gmail.com>
2023-10-24 19:54:22 -0300

    Animal printer scene created

---
+++ Assets/Scripts/AnimalPrinter.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class AnimalPrinter : MonoBehaviour
+    {
+        public TextMeshProUGUI Animal;
+
+        public enum Animals
+        {
+            Crab,
+            Lion,
+            Armadillo
+        }
+
+        void Update()
+        {
+            KeyCode keyPressed = GetKeyPressed();
+            switch (keyPressed)
+            {
+                case KeyCode.A:
+                    Animal.text = Animals.Crab.ToString();
+                    break;
+                case KeyCode.S:
+                    Animal.text = Animals.Lion.ToString();
+                    break;
+                case KeyCode.D:
+                    Animal.text = Animals.Armadillo.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private KeyCode GetKeyPressed()
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+                return KeyCode.A;
+
+            if (Input.GetKeyDown(KeyCode.S))
+                return KeyCode.S;
+
+            if (Input.GetKeyDown(KeyCode.D))
+                return KeyCode.D;
+
+            return KeyCode.None;
+        }
+    }
+}
